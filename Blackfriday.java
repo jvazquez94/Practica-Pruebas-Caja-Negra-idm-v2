c353348fd75f910e7bdc4023025b5027a8c759f3
jvazquez94 <36034121+jvazquez94@users.noreply.github.com>
2020-12-26 14:17:32 +0100

    Add files via upload
    Black Friday implementado

--- Blackfriday.java
+++ Blackfriday.java
@@ -1,4 +1,41 @@
-
+import java.lang.IllegalArgumentException;
 public class Blackfriday {
-
+// @param precioOriginal es el precio de un producto marcado en la etiqueta
+// porcentajeDescuento es el descuento a aplicar expresado como un porcentaje que ha de ser >= 0
+// @return el precio final teniendo en cuenta que si es black friday (29 de noviembre) se aplica
+// un descuento de porcentajeDescuento
+// @throws IllegalArgumentException si precioOriginal es negativo o porcentajeDescuento es negativo
+	
+	private static boolean fechaInco(int fecha) {
+		boolean esIncorrecta=false;
+		int dia = (fecha/100)-((fecha/100)%1);
+		int mes = fecha-(dia*100);
+		
+		//Dia incorecto, ponemos hasta 31 dias de mes
+		if(dia<1 || dia >31) {
+			esIncorrecta=true;
+		//Mes incorrecto
+		}else if(mes<1 || mes>12) {
+			esIncorrecta=true;
+		}
+		return esIncorrecta;
+	}
+	
+	public static double precioFinal
+	(double precioOriginal, double porcentajeDescuento,int fecha) throws IllegalArgumentException{
+	//fecha es un integer 2911 es para el descuento del 29 de noviembre
+		//Empezamos con las caracterizaciones que condicionan al illegal argument
+		double precioFinal = precioOriginal * (porcentajeDescuento/100);
+		
+		if(precioOriginal <= 0) {
+			throw new IllegalArgumentException("Precio original inválido.");
+		}else if(porcentajeDescuento < 0) {
+			throw new IllegalArgumentException("Descuento inválido.");
+		}else if((fecha <= 0) || (fechaInco(fecha)==true)) {
+			throw new IllegalArgumentException("Fecha inválida.");
+		}else if(fecha != 2911) {
+			precioFinal = precioOriginal;
+		}
+		return precioFinal;
+	}
 }
--- BlackfridayTest.java
+++ BlackfridayTest.java
@@ -1,14 +1,74 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 import java.util.*;
+import java.lang.*;
 
-public class BlackfridayTest {
-	/*Este test implementa la combinación de los bloques c1.bx,
-	 *c2.by, c3.bz de las caracterizaciones c1, c2, c3*/
-	
-	//C1:
-	@Test 
-	public void test1() {
+
+public class BlackfridayTest {	
+	//C1:Si no estamos a 29 de noviembre
+	@Test
+	public void testC1() {
+		double pOrig = 10;
+		double d = 50;
+		int f = 2912;
+		double infi = 0.000001;
 		
+		/*Tengo que usar la funcion de java.lan.Double compare para comprobar que los precios son iguales debido a que 
+		 * no estamos en 29 de noviembre por ello uso assertEquals con 0 ya que si es 0 n el compare, es que son iguales
+		 * y -1 si por lo tanto estamos a 29 de noviembre y se aplica el descuento, por lo que no serian iguales*/
+		assertEquals("0 si es igual, -1 si es distinto",0,Double.compare(Blackfriday.precioFinal(pOrig, d, f), pOrig));
+	
 	}
+	
+	//C2.a: Valor negativo o 0 para precio original
+	@Test(expected=IllegalArgumentException.class)
+	public void testC2a() {
+		double pOrig = -10;
+		double d = 50;
+		int f = 2912;
+		
+		Blackfriday.precioFinal(pOrig,d,f);
+	}
+	
+	//C2.b: Valor negativo para descuento
+	@Test(expected=IllegalArgumentException.class)
+	public void testC2b() {
+		double pOrig = 10;
+		double d = -50;
+		int f = 2912;
+		
+		Blackfriday.precioFinal(pOrig,d,f);
+	}
+	
+	//C3: fecha incorrecta
+	//C3.a: fecha negativa o menor 0
+	@Test(expected=IllegalArgumentException.class)
+	public void testC3a() {
+		double pOrig = 10;
+		double d = 50;
+		int f = -2912;
+		
+		Blackfriday.precioFinal(pOrig,d,f);
+	}
+	
+	//C3.b: fecha incorrecta
+	//C3.b.1: fecha con dia incorrecto
+	@Test(expected=IllegalArgumentException.class)
+	public void testC3b1() {
+		double pOrig = 10;
+		double d = 50;
+		int f = 4912;
+		
+		Blackfriday.precioFinal(pOrig,d,f);
+	}
+	
+	//C3.b.2: fecha con mes incorecto
+	@Test(expected=IllegalArgumentException.class)
+	public void testC3b2() {
+		double pOrig = 10;
+		double d = 50;
+		int f = 2915;
+		
+		Blackfriday.precioFinal(pOrig,d,f);
+	}	
 }
