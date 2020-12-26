345d31a2c9f4878b4199c4ce499ec610f17b30c4
jvazquez94 <36034121+jvazquez94@users.noreply.github.com>
2020-12-26 12:22:59 +0100

    Add files via upload
    Embotelladora hecha

--- Embotelladora.java
+++ Embotelladora.java
@@ -9,15 +9,42 @@ class NoSolution extends Exception{
 }
 
 public class Embotelladora {
-// @param pequenas: n´umero de botellas en almac´en de 1L
-// grandes : n´umero de botellas en almac´en de 5L
-// total : n´umero total de litros que hay que embotellar
-// @return n´umero de botellas peque~nas necesarias para embotellar el total de l´ıquido, teniendo
-// en cuenta que hay que minimizar el n´umero de botellas peque~nas: primero
-// se rellenan las grandes
-// @throws NoSolution si no es posible embotellar todo el l´ıquido
-public int calculaBotellasPequenas(int pequenas, int grandes, int total) throws NoSolution {
-//
-}
-
+	// @param pequenas: numero de botellas en almacen de 1L
+	// grandes : numero de botellas en almacen de 5L
+	// total : numero total de litros que hay que embotellar
+	// @return numero de botellas pequeñas necesarias para embotellar el total de liquido, teniendo
+	// en cuenta que hay que minimizar el numero de botellas pequeñas: primero
+	// se rellenan las grandes
+	// @throws NoSolution si no es posible embotellar todo el liquido
+	public static int calculaBotellasPequenas(int pequenas, int grandes, int total) throws NoSolution {
+		int totalg=grandes*5;
+		int totalp=pequenas*1;//Lo dejo identificado como algo simbolico
+		int pusadas=0;
+		int gusadas=0;
+	
+		
+		if(pequenas==0 && grandes==0) {
+			throw new NoSolution("No hay botellas.");
+		}else if(total==0){
+			throw new NoSolution("No se ha definido un total (total = 0).");
+		}else{
+			if((totalg+ totalp) < total) {//Si el total de las grandes y pequeñas no llega al total a embotellas -> No hay solucion
+				throw new NoSolution("No hay suficientes botellas para rellenar el total.");
+			}else if(pequenas==0 && grandes != 0) {//Si no tenemos pequeñas pero si grandes
+				if(totalg%total != 0) {//Luego si ademas no el total de lass grandes no es divisible entre el total
+					throw new NoSolution("Solo hay botellas grandes y no podemos rellenar correctamente el total.");
+				}
+			}else if(pequenas != 0 && grandes != 0) {//Si tenemos grandes y pequeñas
+				if(totalg>total && (totalg%total != 0)) {//Pero el total de las grandes  es mayor que el total a embotellar y no es divisible
+					gusadas=(total/5)-((total/5)%5); //Cogemos el numer de botellas grandes que usaremos (nos quedamos con la parte entera)
+					if(totalp < (total-gusadas*5)){//Y si el total de las pequeñas no  llega al necesario a rellenar del total y las grandes usadas
+						throw new NoSolution("No tenemos botellas pequeñas suficientes para rellenar el resto con las grandes.");
+					}
+				}
+			}
+			gusadas=(total/5)-((total/5)%5);//Las grandes que usamos
+			pusadas = total - gusadas*5;//Las pequeñas que usamos
+			return pusadas;	
+		}
+	}
 }
--- EmbotelladoraTest.java
+++ EmbotelladoraTest.java
@@ -3,12 +3,49 @@ import org.junit.*;
 import java.util.*;
 
 public class EmbotelladoraTest {
-	/*Este test implementa la combinación de los bloques c1.bx,
-	 *c2.by, c3.bz de las caracterizaciones c1, c2, c3*/
 	
-	//C1:
-	@Test 
-	public void test1() {
-		
+	//C1: Que el número de botellas sea insuficiente para rellenar el líquido.
+	@Test(expected=NoSolution.class) 
+	public void testC1() throws NoSolution {
+		int p = 2;
+		int g = 1;
+		int t = 8;
+		Embotelladora.calculaBotellasPequenas(p,g,t);
+	}
+	
+	//C1.b: Que el número de botellas sea 0.
+	@Test (expected=NoSolution.class)
+	public void testC1b() throws NoSolution {
+		int p = 0;
+		int g = 0;
+		int t = 8;
+		Embotelladora.calculaBotellasPequenas(p,g,t);
+	}
+	
+	//C2: Que solo haya botellas grandes y el total no sea múltiplo de 5.
+	@Test (expected=NoSolution.class)
+	public void testC2() throws NoSolution {
+		int p = 0;
+		int g = 3;
+		int t = 12;
+		Embotelladora.calculaBotellasPequenas(p,g,t);
+	}
+	
+	//C2.b: Que haya botellas grandes de sobra pero no pequeñas para llegar al total que no es múltiplo de 5.
+	@Test (expected=NoSolution.class)
+	public void testC2b() throws NoSolution {
+		int p = 2;
+		int g = 2;
+		int t = 8;
+		Embotelladora.calculaBotellasPequenas(p,g,t);
+	}
+	
+	//C3: Que el total a embotellar sea 0
+	@Test (expected=NoSolution.class)
+	public void testC3()throws NoSolution {
+		int p = 2;
+		int g = 1;
+		int t = 0;
+		Embotelladora.calculaBotellasPequenas(p,g,t);
 	}
 }
